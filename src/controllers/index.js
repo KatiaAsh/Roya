95a397d15ca70478966e05f2fd0f3f9d6416338d
katiaash <katia.ashkar7@gmail.com>
2017-10-16 18:33:52 +0300

    create hanoiResult

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -8,12 +8,16 @@ const base = require('./base');
 const generalGuidelines = require('./generalGuidelines');
 const balloonInstructions = require('./balloonInstructions');
 const result = require('./result');
+const hanoiInstructions = require('./hanoiInstructions');
+const hanoiResult = require('./hanoiResult');
 
 // require hbs file
 router.get('/', base);
 router.get('/generalGuidelines', generalGuidelines);
 router.get('/balloonInstructions', balloonInstructions);
 router.get('/result', result);
+router.get('/hanoiInstructions', hanoiInstructions);
+router.get('/hanoiResult', hanoiResult);
 
 // router.use(error.client);
 // router.use(error.server);
